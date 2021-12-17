80998aeb8c80a912fc14488e7bf56ed74ec13af8
SakhSH <shalaginov00@gmail.com>
2021-12-17 23:41:04 +1100

    Push final

--- app/src/main/java/com/example/calculatormobileapp/MainActivity.java
+++ app/src/main/java/com/example/calculatormobileapp/MainActivity.java
@@ -12,14 +12,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NAME_SAVE = "saveTextView";
+
     private TextView textView;
+
+    private boolean sumBoolean;
 
     private double numOne;
     private double numTwo;
     private double numSum;
 
+    private int counter = 0;
 
-    private Button sum, zero, twoZeros, plus, three, two, one,
+
+    private Button sum, zero, plus, three, two, one,
             minus, six, five, four, multiply, nine, eight, seven, divideUp,
             clearText;
 
@@ -34,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
         sum = findViewById(R.id.sum);
         zero = findViewById(R.id.zero);
-        twoZeros = findViewById(R.id.twoZeros);
         plus = findViewById(R.id.plus);
         three = findViewById(R.id.three);
         two = findViewById(R.id.two);
@@ -51,22 +56,34 @@ public class MainActivity extends AppCompatActivity {
         clearText = findViewById(R.id.clearText);
 
         sum.setOnClickListener(view -> sumButton());
-        zero.setOnClickListener(view -> zeroButton());
-        twoZeros.setOnClickListener(view -> twoZerosButton());
+        zero.setOnClickListener(view -> numberButtonClick("0"));
         plus.setOnClickListener(view -> plusButton());
-        three.setOnClickListener(view -> threeButton());
-        two.setOnClickListener(view -> twoButton());
-        one.setOnClickListener(view -> oneButton());
+        three.setOnClickListener(view -> numberButtonClick("3"));
+        two.setOnClickListener(view -> numberButtonClick("2"));
+        one.setOnClickListener(view -> numberButtonClick("1"));
         minus.setOnClickListener(view -> minusButton());
-        six.setOnClickListener(view -> sixButton());
-        five.setOnClickListener(view -> fiveButton());
-        four.setOnClickListener(view -> fourButton());
+        six.setOnClickListener(view -> numberButtonClick("6"));
+        five.setOnClickListener(view -> numberButtonClick("5"));
+        four.setOnClickListener(view -> numberButtonClick("4"));
         multiply.setOnClickListener(view -> multiplyButton());
-        nine.setOnClickListener(view -> nineButton());
-        eight.setOnClickListener(view -> eightButton());
-        seven.setOnClickListener(view -> sevenButton());
+        nine.setOnClickListener(view -> numberButtonClick("9"));
+        eight.setOnClickListener(view -> numberButtonClick("8"));
+        seven.setOnClickListener(view -> numberButtonClick("7"));
         divideUp.setOnClickListener(view -> divideUpButton());
         clearText.setOnClickListener(view -> clearTextButton());
+    }
+
+    private void numberButtonClick(String number) {
+        if(counter == 9){
+            return;
+        }
+
+        if (sumBoolean){
+            textView.setText("");
+            sumBoolean = false;
+        }
+        textView.append(number);
+        counter++;
     }
 
     private void clearTextButton() {
@@ -74,142 +91,153 @@ public class MainActivity extends AppCompatActivity {
         numTwo = 0;
         numOne = 0;
         numSum = 0;
+        counter = 0;
     }
 
     private void divideUpButton() {
 
-        if (textView.length() < 1 | textView.getText().toString().contains("/") | textView.getText().toString().contains("*")
+        if (textView.getText() == null | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
                 | textView.getText().toString().contains("-") | textView.getText().toString().contains("+")) {
             Toast.makeText(this, "Введите число!", Toast.LENGTH_SHORT).show();
         } else {
             numOne = Double.parseDouble(textView.getText().toString().trim());
             textView.append("/");
         }
-    }
 
-    private void sevenButton() {
-        textView.append("7");
+        sumBoolean = false;
+        counter = 0;
 
-    }
-
-    private void eightButton() {
-        textView.append("8");
-    }
-
-    private void nineButton() {
-        textView.append("9");
     }
 
     private void multiplyButton() {
-        if (textView.length() < 1 | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
+        if (textView.getText() == null | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
                 | textView.getText().toString().contains("-") | textView.getText().toString().contains("+")) {
             Toast.makeText(this, "Введите число!", Toast.LENGTH_SHORT).show();
         } else {
             numOne = Double.parseDouble(textView.getText().toString().trim());
             textView.append("*");
         }
-    }
 
-    private void fourButton() {
-        textView.append("4");
-    }
+        sumBoolean = false;
+        counter = 0;
 
-    private void fiveButton() {
-        textView.append("5");
-    }
-
-    private void sixButton() {
-        textView.append("6");
     }
 
     private void minusButton() {
-        if (textView.length() < 1 | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
+        if (textView.getText() == null | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
                 | textView.getText().toString().contains("-") | textView.getText().toString().contains("+")) {
             Toast.makeText(this, "Введите число!", Toast.LENGTH_SHORT).show();
         } else {
             numOne = Double.parseDouble(textView.getText().toString().trim());
             textView.append("-");
         }
-    }
 
-    private void oneButton() {
-        textView.append("1");
-    }
+        sumBoolean = false;
+        counter = 0;
 
-    private void twoButton() {
-        textView.append("2");
-    }
-
-    private void threeButton() {
-        textView.append("3");
     }
 
     private void plusButton() {
-        if (textView.length() < 1 | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
+        if (textView.getText() == null | textView.getText().toString().contains("*") | textView.getText().toString().contains("/")
                 | textView.getText().toString().contains("-") | textView.getText().toString().contains("+")) {
             Toast.makeText(this, "Введите число!", Toast.LENGTH_SHORT).show();
         } else {
             numOne = Double.parseDouble(textView.getText().toString().trim());
             textView.append("+");
         }
-    }
 
-    private void twoZerosButton() {
-        textView.append("00");
-    }
+        sumBoolean = false;
+        counter = 0;
 
-    private void zeroButton() {
-        textView.append("0");
     }
 
     private void sumButton() {
 
         String str = textView.getText().toString().trim();
 
+        if (str.isEmpty()) { return; }
+
         if (str.contains("+")) {
             str = str.substring(str.indexOf("+") + 1);
-            numTwo = Double.parseDouble(str);
-            numSum = numOne + numTwo;
+
+            if(!str.isEmpty()){
+                numTwo = Double.parseDouble(str);
+                numSum = numOne + numTwo;
+            }else {
+                return;
+            }
         } else if (str.contains("-")) {
             str = str.substring(str.indexOf("-") + 1);
-            numTwo = Double.parseDouble(str);
-            numSum = numOne - numTwo;
+
+            if(!str.isEmpty()) {
+                numTwo = Double.parseDouble(str);
+                numSum = numOne - numTwo;
+            }else {
+                return;
+            }
         } else if (str.contains("*")) {
             str = str.substring(str.indexOf("*") + 1);
-            numTwo = Double.parseDouble(str);
-            numSum = numOne * numTwo;
+
+            if(!str.isEmpty()) {
+                numTwo = Double.parseDouble(str);
+                numSum = numOne * numTwo;
+            }else {
+                return;
+            }
         } else if (str.contains("/")) {
 
             str = str.substring(str.indexOf("/") + 1);
-            numTwo = Double.parseDouble(str);
 
-            if (numOne == 0 | numTwo == 0) {
-                Toast.makeText(this, "Произошел взрыв вселейнной!!!", Toast.LENGTH_SHORT).show();
-                clearTextButton();
-            } else {
-                numSum = numOne / numTwo;
+            if(!str.isEmpty()) {
+                numTwo = Double.parseDouble(str);
+
+                if (numOne == 0 | numTwo == 0) {
+                    Toast.makeText(this, "Произошел взрыв вселейнной!!!", Toast.LENGTH_SHORT).show();
+                    clearTextButton();
+                } else {
+                    numSum = numOne / numTwo;
+                }
+            }else {
+                return;
             }
         }
 
         numTwo = Double.parseDouble(str);
 
-
         if (numSum < 100000000000.0) {
             textView.append("=" + numSum);
             textView.setText(String.valueOf(numSum));
-
-            numOne = numSum;
-            numTwo = 0;
-            numSum = 0;
         } else {
             Toast.makeText(this, "Слишком большое число!", Toast.LENGTH_SHORT).show();
             textView.setText("");
-
-            numOne = numSum;
-            numTwo = 0;
-            numSum = 0;
         }
 
+        numOne = numSum;
+        numTwo = 0;
+        numSum = 0;
+
+        sumBoolean = true;
+    }
+
+    // сохранение состояния
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putString(NAME_SAVE, textView.getText().toString().trim());
+        super.onSaveInstanceState(outState);
+    }
+    // получение ранее сохраненного состояния
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String name;
+
+        name = savedInstanceState.getString(NAME_SAVE);
+        textView.setText(name);
+
+        counter = 0;
 
     }
+
 }
